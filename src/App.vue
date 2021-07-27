7dade1bc09fa1cd2a3c7eddfa6ff0301acd53c02
ronypermadi <ronnypermadi1@gmail.com>
2021-07-27 15:41:38 +0700

    add assets

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,37 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+  <div class="wrapper">
+    <Header/>
+    <Navbar/>
+    <!-- content -->
+    <div class="page-wrapper">
+      <div class="container-xl">
+        <div class="mt-2 mb-2" id="app">
+          <router-view />
+        </div>
+      </div>
     </div>
-    <router-view/>
   </div>
 </template>
 
-<style>
+<style scoped>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+  color: rgb(218, 214, 214);
 }
 </style>
+
+<script>
+// @ is an alias to /src
+import Navbar from '@/components/Navbar.vue'
+import Header from '@/components/Header.vue'
+
+export default {
+  name: 'AppComponent',
+  components: {
+    Navbar,Header
+  }
+}
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,37 @@
+<template>
+<!-- header -->
+    <header class="navbar navbar-expand-md navbar-light d-print-none">
+      <div class="container-xl">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar-menu">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <!-- <h1 class="navbar-brand navbar-brand-autodark d-none-navbar-horizontal pe-0 pe-md-3"> -->
+          <h1 class="navbar-brand navbar-brand d-none-navbar-horizontal pe-0 pe-md-3">
+          <router-link to="/">
+            <img src="../../public/dist/img/logo.png" width="110" height="32" alt="Fordev" class="navbar-brand-image"> Fordev
+          </router-link>
+        </h1>
+        <div class="navbar-nav flex-row order-md-last">
+
+          <div class="nav-item dropdown">
+            <a href="#" class="nav-link d-flex lh-1 text-reset p-0" data-bs-toggle="dropdown"
+              aria-label="Open user menu">
+              <span class="avatar avatar-sm" style="background-image: url(./static/avatars/000m.jpg)"></span>
+              <div class="d-none d-xl-block ps-2">
+                <div>User Name</div>
+                <div class="mt-1 small text-muted">Roles</div>
+              </div>
+            </a>
+            <div class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
+              <a href="#" class="dropdown-item">Set status</a>
+              <a href="#" class="dropdown-item">Profile & account</a>
+              <a href="#" class="dropdown-item">Feedback</a>
+              <div class="dropdown-divider"></div>
+              <a href="#" class="dropdown-item">Settings</a>
+              <a href="#" class="dropdown-item">Logout</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+</template>
